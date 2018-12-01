6098f5a5bee90a1e9f29cf1b516eb9e0844c1161
Peter Kovacs <peter@sanebox.com>
2018-12-01 07:30:44 -0500

    Day 1

--- Package.swift
+++ Package.swift
@@ -4,13 +4,13 @@ import PackageDescription
 let package = Package( name: "AdventOfCode2018",
                        products: [
                          .library(name: "AdventOfCode", targets: [ "AdventOfCode" ]),
-                         .executable(name: "Day1", targets: [ "Day1" ])
+                         .executable(name: "Day1", targets: [ "Day1" ]),
                        ],
                        dependencies: [
-                        .package( url: "https://github.com/peterkovacs/FootlessParser.git", .branch( "inout-remainder" ) ),
+                        .package(url: "https://github.com/peterkovacs/FootlessParser.git", .branch( "inout-remainder" )),
                        ],
                        targets: [
-                         .target( name: "AdventOfCode", dependencies: [ "FootlessParser" ] ),
-                         .target( name: "Day1", dependencies: [ "AdventOfCode" ] )
+                         .target(name: "AdventOfCode", dependencies: [ "FootlessParser" ]),
+                         .target(name: "Day1", dependencies: [ "AdventOfCode" ]),
                        ]
                       )
--- Sources/Day1/main.swift
+++ Sources/Day1/main.swift
@@ -1,3 +1,25 @@
 import AdventOfCode
 import FootlessParser
+import CommonCrypto
 
+let signedInteger = { Int($0)! } <^> (extend <^> (char("+") <|> char("-")) <*> oneOrMore(digit))
+let startingFrequency = 0
+let changes = stdin.map { try! FootlessParser.parse(signedInteger, $0) }
+
+print("PART 1")
+print(changes.reduce(startingFrequency, +))
+
+print("PART 2")
+var frequenciesReached = Set<Int>()
+var frequency = 0
+while true {
+  changes.forEach {
+    frequency += $0
+    if frequenciesReached.contains(frequency) {
+      print(frequency)
+      exit(0)
+    }
+
+    frequenciesReached.insert(frequency)
+  }
+}
